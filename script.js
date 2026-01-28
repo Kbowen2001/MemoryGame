21e897c5832cd7e0179e7cede298cfda8947482a
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-01-28 12:58:54 -0700

    ad

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let firstCard, secondCard;
 
 function flipCard() {
     if(lockBoard) return;
+    if (this === firstCard) return;
+
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
@@ -40,7 +42,14 @@ function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+
+    resetBoard();
   }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
